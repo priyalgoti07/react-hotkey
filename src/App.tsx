06f1579d60f9f84b2d0cd3e544a441c4d44ec94b
priyalgoti07 <gotipriyal1996@gmail.com>
2025-01-13 17:59:11 +0530

    mui tab add

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import { Box, Button, Drawer, Tab, Tabs } from '@mui/material'
+// import { useHotkeys } from 'react-hotkeys-hook'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [value, setValue] = React.useState('one');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+  // Hotkey to toggle the drawer
+  const toggleDrawer = (): void => {
+    setIsDrawerOpen(!isDrawerOpen)
+  }
+  // useHotkeys("ctrl+shift+s", toggleDrawer, { enableOnTags: ["INPUT", "TEXTAREA", "SELECT"] } as any);
+  // useHotkeys("esc", () => setIsDrawerOpen(false))
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Button
+        variant="contained"
+        onClick={() => setIsDrawerOpen(true)}
+        style={{ marginBottom: "16px" }}
+      >
+        Open Drawer
+      </Button>
+      {/* Drawer Component */}
+      <Drawer
+        anchor="right"
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+      >
+        <Box sx={{ width: '100%' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="wrapped label tabs example"
+          >
+            <Tab
+              value="one"
+              label="New Arrivals in the Longest Text of Nonfiction that should appear in the next line"
+              wrapped
+            />
+            <Tab value="two" label="Item Two" />
+            <Tab value="three" label="Item Three" />
+          </Tabs>
+        </Box>
+      </Drawer>
+    </div>
   )
 }
 
